7995922ff17feb47f786396c83c4b01ba3568eb3
gustawsi <gsiedlarski@gmail.com>
2025-09-04 17:42:04 +0200

    Add accuracy function

--- lab_1/helper_functions.py
+++ lab_1/helper_functions.py
@@ -3,3 +3,10 @@ import numpy as np
 def mse(x, y, w):
        e = (w @ x) - y   
        return 0.5 * np.mean(e**2)
+
+def accuracy(x, y, w):
+    e = (w @ x)
+    pred = np.where(e>0, 1, 0)
+    true = np.where(y>0, 1, 0)
+    acc = np.count_nonzero(pred == true) / y.size
+    return acc
